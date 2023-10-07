8c0b5022310db8c476fc2fdceee4bce3b402c989
unknown <gokul8506@gmail.com>
2023-10-06 23:45:19 -0700

    first commit

---
+++ src/pages/Shipping/shipping.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './ProgressBar.css';
+
+const ProgressBar = () => {
+  const [apiData, setApiData] = useState({ status: '' });
+
+  useEffect(() => {
+    // Replace 'your-api-url-here' with the actual URL of your API endpoint
+    fetch('http://172.22.81.182:8080/rfid/getstat/12')
+      .then((response) => response.text())
+      .then((data) => {
+        // Assuming the API response contains only the plain text "REACHED"
+        setApiData({ status: data.trim() });
+      })
+      .catch((error) => {
+        console.error('Error fetching data from API:', error);
+      });
+  }, []);
+
+  const labels = ['DISPATCHED', 'REACHED', 'REACHED HUB 1', 'REACHED HUB 2', 'OUTFORDEL'];
+
+  return (
+    <div className="progress-bar">
+      <div className="progress-labels">
+        {labels.map((label) => (
+          <div
+            key={label}
+            className={`progress-label ${apiData.status === label ? 'active' : ''}`}
+          >
+            {label}
+          </div>
+        ))}
+      </div>
+      <div className={`progress-bar ${apiData.status}`} />
+      {apiData.status === 'OUTFORDEL' && (
+        <button className="bg-amber-400 hover:bg-amber-300 rounded-full h-11 w-24  text-black font-semibold   mt-5">
+          Track
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ProgressBar;
